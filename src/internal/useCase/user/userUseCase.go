ec9a1c3a730b9140c177f8bc496ddf49cbc2115b
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-01-11 12:34:29 -0300

    Implementar interface de caso de uso de usuário

--- src/internal/useCase/user/userUseCase.go
+++ src/internal/useCase/user/userUseCase.go
@@ -1,13 +1,22 @@
 package user
 
 import (
-	"github.com/Fernando-hub527/candieiro/internal/entity"
+	"fmt"
+
 	"github.com/Fernando-hub527/candieiro/internal/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type IUserUseCase interface {
 	ValidAccess(userName string, plantId primitive.ObjectID) *errors.RequestError
-	CreateUser(entity.User) error
-	liberarPlantaParaUsuario(userName string, plantId primitive.ObjectID)
+	// CreateUser(entity.User) error
+	// liberarPlantaParaUsuario(userName string, plantId primitive.ObjectID)
+}
+
+type UserUseCase struct {
+}
+
+func (*UserUseCase) ValidAccess(userName string, plantId primitive.ObjectID) *errors.RequestError {
+	fmt.Println("validando acesso", userName, plantId)
+	return nil
 }
