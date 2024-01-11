417a24197e6bf19c761b9760f2ce1ed07c8afaaa
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-01-11 12:39:26 -0300

    refatorar consumo de filas
    passar o o consumo das filas para dentro dos handles

--- src/internal/cmd/main.go
+++ src/internal/cmd/main.go
@@ -1,40 +1,28 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Fernando-hub527/candieiro/internal/api"
+	"github.com/Fernando-hub527/candieiro/internal/pkg/mongodb"
 	"github.com/Fernando-hub527/candieiro/internal/pkg/rabbitmq"
 	"github.com/labstack/echo/v4"
 	"github.com/rabbitmq/amqp091-go"
 )
 
-func a() {
-
-}
-
 func main() {
-	validators := map[string]func(){
-		"time": a,
-	}
-	b := validators["times"]
-	fmt.Println(b == nil)
-	// chanelSensors := make(chan amqp091.Delivery)
-
-	// mongodb.SetUpDatabase("mongodb://root:example@localhost:27018/")
-	// startBroker(chanelSensors, "amqp://iot:iot@localhost:5673/")
-	// startApi(chanelSensors)
+	mongodb.SetUpDatabase("mongodb://root:example@localhost:27018/")
+	chRabbit := startBroker("amqp://iot:iot@localhost:5673/")
+	startApi(chRabbit)
 }
 
-func startBroker(chSensors chan amqp091.Delivery, url string) {
+func startBroker(url string) *amqp091.Channel {
 	ch, err := rabbitmq.OpenChannel(url)
 	if err != nil {
 		panic(err)
 	}
-	go rabbitmq.Consumer(chSensors, ch, "sensors")
+	return ch
 }
 
-func startApi(chSensors chan amqp091.Delivery) {
+func startApi(chSensors *amqp091.Channel) {
 	server := echo.New()
 	api.SetRouts(chSensors, api.SetWebsocket(server), server)
 	server.Logger.Fatal(server.Start(":1323"))
