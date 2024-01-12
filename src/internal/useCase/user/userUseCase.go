819b9c6c50435ee0477429562d8bbed36a15e8b3
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-01-12 15:46:13 -0300

    implementar validação de de acesso

--- src/internal/useCase/user/userUseCase.go
+++ src/internal/useCase/user/userUseCase.go
@@ -1,22 +1,40 @@
 package user
 
 import (
-	"fmt"
+	"context"
 
 	"github.com/Fernando-hub527/candieiro/internal/pkg/errors"
+	userrepository "github.com/Fernando-hub527/candieiro/internal/repository/userRepository"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type IUserUseCase interface {
-	ValidAccess(userName string, plantId primitive.ObjectID) *errors.RequestError
+	ValidAccess(userName string, plantId primitive.ObjectID, ctx context.Context) *errors.RequestError
 	// CreateUser(entity.User) error
 	// liberarPlantaParaUsuario(userName string, plantId primitive.ObjectID)
 }
 
 type UserUseCase struct {
+	repository userrepository.IUserRepository
 }
 
-func (*UserUseCase) ValidAccess(userName string, plantId primitive.ObjectID) *errors.RequestError {
-	fmt.Println("validando acesso", userName, plantId)
-	return nil
+func NewUserCase(repository userrepository.IUserRepository) *UserUseCase {
+	return &UserUseCase{
+		repository: repository,
+	}
+}
+
+func (useCase *UserUseCase) ValidAccess(userName string, plantId primitive.ObjectID, ctx context.Context) *errors.RequestError {
+	user, err := useCase.repository.FindUserByNameOrEmail(userName, "", ctx)
+	if err != nil {
+		return errors.NewErrorAccessDenied("Unregistered user")
+	}
+
+	for _, plan := range user.PlanId {
+		if plan == plantId {
+			return nil
+		}
+	}
+
+	return errors.NewErrorAccessDenied("Unregistered user")
 }
