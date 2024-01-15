6f905b3c6a936fd0b696a78b3a6de60d44727a44
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-01-15 12:40:32 -0300

    implementar web socket
    implementar hub

--- src/internal/pkg/websocket/hub.go
+++ src/internal/pkg/websocket/hub.go
@@ -1,13 +1,15 @@
 package websocket
 
-type Hub struct {
-	clients map[*Client]bool
+import "github.com/Fernando-hub527/candieiro/internal/pkg/utils"
 
-	register chan *Client
+type Hub struct {
+	Romms map[string][]*Client
+
+	registers chan map[*Client][]string
 
 	unregister chan *Client
 
-	messages chan []byte
+	messages chan map[string][]byte
 }
 
 /*
@@ -17,13 +19,53 @@ retornar o endereço de uma variavel que quando a função acabar, teoricamente,
 */
 func NewHub() *Hub {
 	return &Hub{
-		clients:    make(map[*Client]bool),
-		register:   make(chan *Client),
+		Romms:      map[string][]*Client{},
+		registers:  make(chan map[*Client][]string),
 		unregister: make(chan *Client),
-		messages:   make(chan []byte),
+		messages:   make(chan map[string][]byte),
 	}
 }
 
 func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.registers:
+			h.Romms = addClientsToRoom(client, h.Romms)
+		case client := <-h.unregister:
+			h.Romms = removeClientToRooms(client, h.Romms)
+		case messages := <-h.messages:
+			senMessage(messages, h.Romms)
+		}
+	}
 
+}
+
+func senMessage(messages map[string][]byte, rooms map[string][]*Client) {
+	for room, message := range messages {
+		for _, client := range rooms[room] {
+			client.send <- message
+		}
+	}
+}
+
+func addClientsToRoom(client map[*Client][]string, rooms map[string][]*Client) map[string][]*Client {
+	for client, clientRooms := range client {
+		for _, room := range clientRooms {
+			rooms[room] = append(rooms[room], client)
+			return rooms
+		}
+	}
+	return rooms
+}
+
+func removeClientToRooms(client *Client, rooms map[string][]*Client) map[string][]*Client {
+	for _, clientRoom := range client.rooms {
+		rooms[clientRoom] = utils.RemoveItemFromSlice2[*Client](rooms[clientRoom], client, func(itemA, itemB *Client) bool { return itemA.id == itemB.id })
+		if len(rooms[clientRoom]) == 0 {
+			delete(rooms, clientRoom)
+		}
+		close(client.send)
+		return rooms
+	}
+	return rooms
 }
