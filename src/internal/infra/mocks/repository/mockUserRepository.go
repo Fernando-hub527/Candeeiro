172f91cd388683c555683ccc9b09dfefab1f125b
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-02-04 09:33:19 -0300

    Testes unitários
    Criação de mocks de repositorio

--- src/internal/infra/mocks/repository/mockUserRepository.go
+++ src/internal/infra/mocks/repository/mockUserRepository.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Fernando-hub527/candieiro/internal/entity"
 	"github.com/Fernando-hub527/candieiro/internal/pkg/errors"
+	"github.com/Fernando-hub527/candieiro/internal/pkg/utils"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -18,15 +19,10 @@ func NewMockUserRepository() *mockUserRepository {
 
 func (mock *mockUserRepository) FindUserByNameOrEmail(name string, email string, ctx context.Context) (*entity.User, *errors.RequestError) {
 	args := mock.Called(name, email, ctx)
-	return parseType[entity.User](args.Get(0)), parseType[errors.RequestError](args.Get(1))
+	return utils.ParseType[entity.User](args.Get(0)), utils.ParseType[errors.RequestError](args.Get(1))
 }
 
 func (mock *mockUserRepository) CreateUser(user entity.User, ctx context.Context) *errors.RequestError {
 	args := mock.Called(user, ctx)
 	return args.Get(0).(*errors.RequestError)
-}
-
-func parseType[T interface{}](object interface{}) *T {
-	obj, _ := object.(*T)
-	return obj
 }
