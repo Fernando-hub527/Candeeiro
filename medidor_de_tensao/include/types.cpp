2cd40df7b29dfac3a1b98b0e01261b433710e525
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-02-07 23:45:23 -0300

    Definição de estruturas básicas do programa

--- medidor_de_tensao/include/types.cpp
+++ medidor_de_tensao/include/types.cpp
@@ -4,10 +4,22 @@
 
 typedef void(*UpdateErrorCallback)(String, String);
 
-struct consumptionRecord
-{
-    /* data */
-};
+struct ConsumptionRecord{
+    unsigned long startConsumption;
+    unsigned long endConsumption;
+    int fullPower;
+    int kwh;
+} consumptionRecord;
 
+struct Settings{
+    const char *ssid;
+    const char *password;
+    const char *serialNumber;
+    const char *ipBroker;
+    int brokerDoor;
+    int measurementTime;
+}settings;
+
+const char* fileSetting = "fileSettings.txt";
 
 #endif
