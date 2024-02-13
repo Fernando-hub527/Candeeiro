46399f1a5f06f90e27ceec8426e99133e52cf4c0
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-02-13 00:21:17 -0300

    Remoção de acesso a memória interna

--- medidor_de_tensao/include/utils/db.cpp
+++ medidor_de_tensao/include/utils/db.cpp
@@ -7,14 +7,11 @@
 
 Settings loadSetting(const char* fileSettings){
     File file = LittleFS.open(fileSetting, "r");
-    if(!file){
-        Serial.println("Falha ao carregar dados de configuração");
-        return {"Saraiva", "9116219d", 159753, "192.57.1.6", "device/electricity/consumption/full", "device/electricity/consumption/current", 1883, 5};
-    }
+    Serial.println("Falha ao carregar dados de configuração");
+    return {"Saraiva", "9116219d", 159753, "192.57.1.6", "device/electricity/consumption/full", "device/electricity/consumption/current", 1883, 5};
+    
 
-    while(file.available()){
-        
-    }
+
 }
 
 #endif
