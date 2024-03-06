faa32a9c06a1a5e7654a3d8d2b794ffdc69b85f3
Fernando-hub527 <fernandocoelhosaraivanando@gmail.com>
2024-03-06 00:35:17 -0300

    Desenvolvimento de tela de login

--- front/src/pages/login/LoginPage.js
+++ front/src/pages/login/LoginPage.js
@@ -1,23 +1,42 @@
 import "../../static/css/styleLoginPage.css"
 import "@fontsource/comfortaa"
+import arrowIcon from "../../static/img/arrowIcon.svg"
+import eyeIcon from "../../static/img/eyeIcon.svg"
+import { useState } from "react"
 
 export function LoginPage(props) {
+    const [passwordVisibility, setVisibility] = useState(false)
+
+
     return <div className="page_login">
         <div className="login_header">
-            <h1>Candieiro</h1>
+            <img src={arrowIcon} alt="Um icone preto de uma seta apontada para a esquerda"/>
+            <h2>Login</h2>
         </div>
         <form>
             <div className="form_input form_user">
                 <input type="text" id="nickName" placeholder="Usuário" />
             </div>
             <div className="form_input form_password">
-                <input type="password" id="password" placeholder="Senha" />
+                <input type={passwordVisibility ? "text":"password"} id="password" placeholder="Senha" />
+                <div className="form_password_eye" onClick={()=>setVisibility((state)=> !state)}>
+                    <hr className={`form_password_eye-${passwordVisibility ? "visible" : "invisible"}`}/>
+                    <img draggable={false} src={eyeIcon} alt="Icone roxo de um olho indicando se a senha está visível"/>
+                </div>
             </div>
-            <div className="form_btn">
-                <button className="btn btn-input">LOG IN</button>
-                <button className="btn btn-register">REGISTER</button>
-            </div>
+            <button className="btn btn_input">Entrar</button>
         </form>
+        <div className="form_division">
+            <hr />
+            <p>ou</p>
+            <hr />
+        </div>
+
+        <div className="login_options">
+            <button className="btn login_option">Continuar com google</button>
+            <button className="btn login_option"><img src={eyeIcon}/>Continuar com linkedin</button>
+
+        </div>
     </div>
 
 
--- front/src/pages/presentation/Presentation.js
+++ front/src/pages/presentation/Presentation.js
@@ -1,7 +1,13 @@
 import personIcon from "../../static/img/personIcon.png"
 import "../../static/css/stylePresentation.css"
+import { useNavigate } from "react-router-dom";
 
 export function Presentation(props){
+    const navigate = useNavigate();
+    
+    console.log(props)
+    const openScreen = (path) => setTimeout(()=>navigate(path), 150)
+
     return <div className="page_presentation">
         <header>
             <h1>Bem-vindo</h1>
@@ -10,7 +16,7 @@ export function Presentation(props){
         <img className="page_presentation_user" src={personIcon} alt="icone de uma pessoa vestindo blusa roxa e short amarelo enquanto equilibra equipamentos eletrônicos"/>
        
         <div className="form_btn">
-            <button className="btn btn-login">LOG IN</button>
+            <button className="btn btn-login" onClick={() => openScreen("/login")}>LOG IN</button>
             <button className="btn btn-register">REGISTER</button>
         </div>
 
